29a95498ec51cb50ed28a9d990d12e4f4d19e803
Anastasia Ease <easeanas@myvuw.ac.nz>
2021-06-08 17:17:14 +1200

    Reformat Game class

--- src/Snake/Game.java
+++ src/Snake/Game.java
@@ -1,32 +1,18 @@
 package snake;
+
 import java.util.ArrayList;
 
 import application.GUI;
 import render.Board;
 
 /**
- * The game class stores information about the state of the game.
- * This includes storing the star, the tiles, the board, and the snake. 
+ * The game class stores information about the state of the game. This includes
+ * storing the star, the tiles, the board, and the snake.
  * 
  * @author Anastasia Ease
  *
  */
 public class Game {
-	private static Star star;
-	private Board board;
-	private static Snake snake;
-	private  ArrayList<ArrayList<Tile>> tiles;
-	
-	/**
-	 * The directions of movement the player can make.
-	 */
-	public enum Direction{
-		RIGHT,
-		LEFT,
-		UP,
-		DOWN
-	}
-	
   private static Star star;
   private Board board;
   private static Snake snake;
@@ -71,32 +57,32 @@ public class Game {
     System.out.println("****\n"+tiles);
   }
 
-	/**
-	 * Gets the star.
-	 * 
-	 * @return the star
-	 */
-	public static Star getStar() {
-		return star;
-	}
+  /**
+   * Gets the star.
+   * 
+   * @return the star
+   */
+  public static Star getStar() {
+    return star;
+  }
 
-	/**
-	 * Gets the snake object.
-	 * 
-	 * @return the snake
-	 */
-	public static Snake getSnake() {
-		return snake;
-	}
+  /**
+   * Gets the snake object.
+   * 
+   * @return the snake
+   */
+  public static Snake getSnake() {
+    return snake;
+  }
 
-	/**
-	 * Gets the tile at the given position.
-	 * 
-	 * @param row
-	 * @param col
-	 * @return tile at row, col 
-	 */
-	public Tile getTile(int row, int col) {
-		return this.tiles.get(row).get(col);
-	}
+  /**
+   * Gets the tile at the given position.
+   * 
+   * @param row
+   * @param col
+   * @return tile at row, col
+   */
+  public Tile getTile(int row, int col) {
+    return this.tiles.get(row).get(col);
+  }
 }
